46d7041c74fd66fe466b93581d5c683d5d884ee4
Mike James <m@mikejam.es>
2017-03-22 23:43:01 +1100

    basic snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders init correctly', () => {
+  const tree = renderer.create(
+		<App />
+	).toJSON();
+	expect(tree).toMatchSnapshot();
+})
